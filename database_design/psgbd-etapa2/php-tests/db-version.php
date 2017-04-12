adb7305285ede85f060d67e01787fa1c43ff016a
Adrian Harabulă <adrian.harabula@gmail.com>
2017-04-12 23:27:01 +0300

    removes unnecesary test files

--- database_design/psgbd-etapa2/php-tests/db-version.php
+++
@@ -1,7 +0,0 @@
-<?php
-$config = include('../config.php');
-$conn = oci_connect($config->user, $config->pass, $config->host.':'.$config->port.'/'.$config->sid);
-
-echo "Client Version: " . oci_client_version();
-echo "<br />";
-echo "Server Version: " . oci_server_version($conn);
