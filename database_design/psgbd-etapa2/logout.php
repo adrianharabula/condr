de5da2923fa1e327d4b146741dd3c78b738ea48d
Adrian Harabulă <adrian.harabula@gmail.com>
2017-04-14 12:22:45 +0300

    fixes logout not redirecting

--- database_design/psgbd-etapa2/logout.php
+++ database_design/psgbd-etapa2/logout.php
@@ -1,19 +1,5 @@
 <?php
-require('autoload.php');
-
-$pageTitle = "Log Out";
-require('Parts/header.php');
-?>
-
-  <div class="row">
-		<div class="col-md-4 col-md-offset-4">
-        <div class="panel panel-success">
-        	<div class="panel-heading">Logout succesful!</div>
-        </div>
-    </div>
-  </div>
-
-<?php require('Parts/footer.php'); ?>
-
-<?php
+session_start();
 session_destroy();
+header("location:/index.php");
+exit();
