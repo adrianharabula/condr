3793b89b21ea04f775fea022f305528f82f4e531
Adrian Harabulă <adrian.harabula@gmail.com>
2017-04-14 10:18:35 +0300

    ads index.php page

---
+++ database_design/psgbd-etapa2/App/index.php
@@ -0,0 +1,16 @@
+<?php
+require('../autoload.php');
+
+$pageTitle = "Welcome Page";
+require('../Parts/header.php');
+?>
+
+  <div class="row">
+		<div class="col-md-4 col-md-offset-4">
+        <div class="panel panel-default">
+        	<div class="panel-heading">Welcome! This is first page!</div>
+        </div>
+    </div>
+  </div>
+
+<?php require('../Parts/footer.php'); ?>
