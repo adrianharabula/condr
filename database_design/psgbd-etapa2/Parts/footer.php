c1f5b1041babd116a918fd5b18460461a824b5ba
Adrian Harabulă <adrian.harabula@gmail.com>
2017-04-14 09:24:27 +0300

    ads templating parts for frontend

---
+++ database_design/psgbd-etapa2/Parts/footer.php
@@ -0,0 +1,8 @@
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script type="text/javascript" src="/Assets/js/jquery.1.11.1.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script type="text/javascript" src="/Assets/js/bootstrap.js"></script>
+  </body>
+</html>
---
+++ database_design/psgbd-etapa2/Parts/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?=$pageTitle?></title>
+		<link href="/Assets/css/bootstrap.min.css" rel="stylesheet">
+		<link href="/Assets/css/datepicker3.css" rel="stylesheet">
+		<link href="/Assets/css/styles.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css"  href="/Assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="/Assets/fonts/font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" type="text/css"  href="/Assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="/Assets/css/responsive.css">
+    <script type="text/javascript" src="/Assets/js/modernizr.custom.js"></script>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:500,600,700,100,800,900,400,200,300' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Playball' rel='stylesheet' type='text/css'>
+
+  </head>
+  <body background="/Assets/img/bp_background_2_blue1.jpg">
