c69a36ef94fe4859ebb4d4c487ab56ab5e9bcb63
elis47 <elisa.aioanei@yahoo.com>
2017-04-19 17:13:58 +0300

    Uploaded new menu

--- database_design/psgbd-etapa2/index.php
+++ database_design/psgbd-etapa2/index.php
@@ -5,12 +5,21 @@ $pageTitle = "Welcome Page";
 require('Parts/header.php');
 ?>
 
-  <div class="row">
-		<div class="col-md-4 col-md-offset-4">
-        <div class="panel panel-default">
-        	<div class="panel-heading">Welcome! This is first page!</div>
+<div id="tf-home">
+    <div class="overlay">
+        <div id="sticky-anchor"></div>
+
+
+        <div class="container">
+            <div class="content">
+                <h3 id="main-logo">ConDr</h3>
+                <h3>Good decisions have never been so easy to take!</h3>
+                <br>
+                <a href="register.php" class="btn btn-primary my-btn">Let's get started!</a>
+                <a href="contact.php" class="btn btn-primary my-btn2">Contact us!</a>
+            </div>
         </div>
     </div>
-  </div>
+</div>
 
 <?php require('Parts/footer.php'); ?>
