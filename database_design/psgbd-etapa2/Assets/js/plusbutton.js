ec809b8a7d3280411126fb7dc0f8384254d6432b
Adrian Harabula <adrian.harabula@gmail.com>
2017-04-21 15:47:06 +0300

    ads fix for dynamic button #29

--- database_design/psgbd-etapa2/Assets/js/plusbutton.js
+++ database_design/psgbd-etapa2/Assets/js/plusbutton.js
@@ -7,20 +7,16 @@ $(document).ready(function(){
         next = next + 1;
         var newIn = '<input autocomplete="off" class="input" id="field' + next + '" name="field' + next + '" type="text">';
         var newInput = $(newIn);
-        var removeBtn = '<button id="remove' + (next - 1) + '" class="btn btn-danger remove-me" >-</button>';
-        var newDiv = $( "<div id='fieldd"+next+"' class='form-group row'></div>" );
+        var removeBtn = '<button id="remove_' + (next - 1) + '" class="btn btn-danger remove-me" >-</button><div></div>';
         var removeButton = $(removeBtn);
-
-        $("#fieldd"+(next-1)).after(newDiv);
-        $("#fieldd"+(next)).append(newInput);
-        $("#fieldd"+(next)).append(removeBtn);
-
+        $(addto).after(newInput);
+        $(addRemove).after(removeButton);
         $("#field" + next).attr('data-source',$(addto).attr('data-source'));
         $("#count").val(next);
 
             $('.remove-me').click(function(e){
                 e.preventDefault();
-                var fieldNum = this.id.charAt(this.id.length-1);
+                var fieldNum = parseInt ( this.id.split('_')[1]  );
                 var fieldID = "#field" + fieldNum;
                 $(this).remove();
                 $(fieldID).remove();
--- database_design/psgbd-etapa2/preferencesTest.php
+++ database_design/psgbd-etapa2/preferencesTest.php
@@ -4,17 +4,21 @@ $pageTitle = "Preferences";
 require('Parts/header.php');
 ?>
 
-            <div class="form-group">
-              <label>...or enter your own wanted preferences!*</label>
-              <input type="hidden" name="count" value="1" />
-              <div class="control-group" id="fields">
-                  <div class="controls" id="profs">
-                      <form class="input-append">
-                          <div id="fieldd1" class=''><input autocomplete="off" class="input" id="field1" name="prof1" type="text" placeholder="Type something" data-items="8"/><button id="b1" class="btn add-more" type="button">+</button></div>
-                      </form>
-                  </div>
-              </div>
+<div class="container">
+	<div class="row">
+		<input type="hidden" name="count" value="1" />
+        <div class="control-group" id="fields">
+            <label class="control-label" for="field1">Nice Multiple Form Fields</label>
+            <div class="controls" id="profs">
+                <form class="input-append">
+                    <div id="field"><input autocomplete="off" class="input" id="field1" name="prof1" type="text" placeholder="" data-items="8"/><button id="b1" class="btn add-more" type="button">+</button></div>
+                </form>
+            <br>
+            <small>Press + to add another form field :)</small>
             </div>
+        </div>
+	</div>
+</div>
 
 
 <?php require('Parts/footer.php'); ?>
