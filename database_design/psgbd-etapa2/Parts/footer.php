45207d931f4115e186f9fd8561e750a73d78f631
Adrian Harabula <adrian.harabula@gmail.com>
2017-04-23 09:16:02 +0300

    fix css and js broken links on https

--- database_design/psgbd-etapa2/Parts/footer.php
+++ database_design/psgbd-etapa2/Parts/footer.php
@@ -1,6 +1,6 @@
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script type="text/javascript" src="/Assets/js/jquery.1.11.1.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script type="text/javascript" src="/Assets/js/bootstrap.js"></script>
