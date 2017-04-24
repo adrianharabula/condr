7afdb4c45bd6ed946df47d87196c31b397565ebd
Adrian Harabula <adrian.harabula@gmail.com>
2017-04-24 04:34:21 +0300

    fixes for register page on mobile

--- database_design/psgbd-etapa2/register.php
+++ database_design/psgbd-etapa2/register.php
@@ -34,52 +34,48 @@ require('Parts/header.php');
 require('Parts/headerMenu.php');
 ?>
 
-<?php if ($_REQUEST['submitRegister']): ?>
-  <div class="row">
-		<div class="col-md-4 col-md-offset-4">
-      <?php if ($registerResult): ?>
-        <div class="panel panel-success">
-        	<div class="panel-heading">Register succesful!</div>
-        </div>
-      <?php else: ?>
-        <div class="panel panel-danger">
-  				<div class="panel-heading">Register error!</div>
-        </div>
-      <? endif; ?>
-    </div>
-  </div>
-<?php endif; ?>
+<div class="container">
+  <div class="row content-custom black">
 
-<?php if(!isset($_SESSION['username'])) : ?>
-  <div class="row">
-    <div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
-      <div class="login-panel panel panel-default">
-        <div class="panel-heading">Register </div>
-        <div class="panel-body">
-          <form role="form" action="register.php" method="post">
-            <fieldset>
-              <div class="form-group">
-                <input class="form-control" placeholder="Choose your username" name="username" type="username" autofocus="">
-              </div>
-              <div class="form-group">
-                <input class="form-control" placeholder="Password" name="password" type="password" value="">
-              </div>
-              <div class="form-group">
-                <input class="form-control" placeholder="E-mail address" name="email" type="email" autofocus="">
-              </div>
-              <div class="checkbox">
-                <label>
-                  <input name="remember" type="checkbox" value="Remember Me">Remember Me
-                </label>
-              </div>
-              <input name="submitRegister" class="btn btn-primary" type="submit" value="Register!">
-            </fieldset>
-          </form>
-        </div>
+  <?php if ($_REQUEST['submitRegister']): ?>
+  <div class="col-md-4 col-md-offset-4">
+    <?php if ($registerResult): ?>
+      <div class="panel panel-success">
+      	<div class="panel-heading">Register succesful!</div>
       </div>
-    </div><!-- /.col-->
-  </div><!-- /.row -->
+    <?php else: ?>
+      <div class="panel panel-danger">
+  			<div class="panel-heading">Register error!</div>
+      </div>
+    <? endif; ?>
+  </div>
+  <?php endif; ?>
 
-<?php endif; ?>
+  <?php if(!isset($_SESSION['username'])) : ?>
+  <div class="col-md-4 col-md-offset-4">
+    <div class="login-panel panel panel-warning">
+      <div class="panel-heading">Register </div>
+      <div class="panel-body">
+        <form role="form" action="register.php" method="post">
+          <fieldset>
+            <div class="form-group">
+              <input class="form-control" placeholder="Choose your username" name="username" type="username" autofocus="">
+            </div>
+            <div class="form-group">
+              <input class="form-control" placeholder="Password" name="password" type="password" value="">
+            </div>
+            <div class="form-group">
+              <input class="form-control" placeholder="E-mail address" name="email" type="email" autofocus="">
+            </div>
+            <input name="submitRegister" class="btn btn-block btn-primary my-btn btn-start my-btn-dropdown" type="submit" value="Register!">
+          </fieldset>
+        </form>
+      </div>
+    </div>
+  </div><!-- /.col-->
+  <?php endif; ?>
+
+</div>
+</div>
 
 <?php require('Parts/footer.php'); ?>
