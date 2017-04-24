7b49aa607bc31aa95500b41659db7f76cf6c5316
Adrian Harabula <adrian.harabula@gmail.com>
2017-04-24 04:34:07 +0300

    fixes for login page on mobile

--- database_design/psgbd-etapa2/login.php
+++ database_design/psgbd-etapa2/login.php
@@ -28,48 +28,49 @@ require('Parts/header.php');
 require('Parts/headerMenu.php');
 ?>
 
-<?php if ($_REQUEST['submitLogin']): ?>
-  <div class="row">
-		<div class="col-md-4 col-md-offset-4">
+<div class="container">
+  <div class="row content-custom black">
+
+    <?php if ($_REQUEST['submitLogin']): ?>
+    <div class="col-md-4 col-md-offset-4">
       <?php if ($loginResult): ?>
         <div class="panel panel-success">
         	<div class="panel-heading">Login succesful!</div>
         </div>
       <?php else: ?>
         <div class="panel panel-danger">
-  				<div class="panel-heading">Login error!</div>
+    			<div class="panel-heading">Login error!</div>
         </div>
       <? endif; ?>
     </div>
-  </div>
-<?php endif; ?>
+    <?php endif; ?>
 
-<?php if(!isset($_SESSION['username'])) : ?>
-<div class="row">
-  <div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
-    <div class="login-panel panel panel-default">
-      <div class="panel-heading">Log in</div>
-      <div class="panel-body">
-        <form role="form" action="login.php" method="post">
-          <fieldset>
-            <div class="form-group">
-              <input class="form-control" placeholder="Enter your username" name="username" type="username" autofocus="">
-            </div>
-            <div class="form-group">
-              <input class="form-control" placeholder="Password" name="password" type="password" value="">
-            </div>
-            <div class="checkbox">
-              <label>
-                <input name="remember" type="checkbox" value="Remember Me">Remember Me
-              </label>
-            </div>
-            <input name="submitLogin" class="btn btn-primary" type="submit" value="Log in">
-          </fieldset>
-        </form>
+    <?php if(!isset($_SESSION['username'])) : ?>
+    <div class="col-md-4 col-md-offset-4">
+      <div class="login-panel panel panel-info">
+        <div class="panel-heading">Log in</div>
+        <div class="panel-body">
+          <form role="form" action="login.php" method="post">
+            <fieldset>
+              <div class="form-group">
+                <input class="form-control" placeholder="Enter your username" name="username" type="username" autofocus="">
+              </div>
+              <div class="form-group">
+                <input class="form-control" placeholder="Password" name="password" type="password" value="">
+              </div>
+              <div class="checkbox">
+                <label>
+                  <input name="remember" type="checkbox" value="Remember Me">Remember Me
+                </label>
+              </div>
+              <input name="submitLogin" class="btn btn-block btn-primary my-btn btn-start my-btn-dropdown" type="submit" value="Log in">
+            </fieldset>
+          </form>
+        </div>
       </div>
-    </div>
-  </div><!-- /.col-->
-</div><!-- /.row -->
-<?php endif; ?>
+    </div><!-- /.col-->
+    <?php endif; ?>
+  </div><!-- /.row -->
+</div>
 
 <?php require('Parts/footer.php'); ?>
