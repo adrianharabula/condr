df88e60e84870bf778a462633dc9bb7011da49e1
Adrian Harabula <adrian.harabula@gmail.com>
2017-04-24 08:12:23 +0300

    updates my details page

--- database_design/psgbd-etapa2/mydetails.php
+++ database_design/psgbd-etapa2/mydetails.php
@@ -3,8 +3,6 @@ require('autoload.php');
 require('Parts/header.php');
 require('Parts/headerMenu.php');
 
-
-
 $db = new Database\Database;
 $utils = new Utils\Utils;
 
@@ -13,32 +11,32 @@ $username = $_SESSION['username'];
 $db->query("SELECT * FROM USERS WHERE username = :u" );
 $db->bind(":u", $username);
 $details = $db->execute()->result();
-
 ?>
 
-
-<div class="row">
-    <div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
-      <div class="login-panel panel panel-default">
-        <div class="panel-heading"><b>Details</b></div>
-        <div class="panel-body">
-            <fieldset>
-              <?php foreach($details as $user) : ?>
-                  <div class="form-group">
-                      <div class="form-group">
-                         <p><span style="color:green"><b>Username : </b></span><?=$user->USERNAME?></p>
-                      </div>
-                      <div class="form-group">
-                         <p><span style="color:green"><b>Email : </b></span><?=$user->EMAIL?> </p>
-                      </div>
-                  </div>
-              <?php endforeach ?>
-              <div class="col-md-2"><a href="editpassword.php" class="btn btn-primary my-btn">Edit Password</a></div>
-            </fieldset>
+<div class="container">
+  <div class="row page black">
+      <div class="col-md-4 col-md-offset-4">
+        <div class="login-panel panel panel-default">
+          <div class="panel-heading"><b>Details</b></div>
+          <div class="panel-body">
+              <fieldset>
+                <?php foreach($details as $user) : ?>
+                    <div class="form-group">
+                        <div class="form-group">
+                           <p><span style="color:green"><b>Username: </b></span><?=$user->USERNAME?></p>
+                        </div>
+                        <div class="form-group">
+                           <p><span style="color:green"><b>Email: </b></span><?=$user->EMAIL?> </p>
+                        </div>
+                    </div>
+                <?php endforeach ?>
+                <div><a href="editpassword.php" class="btn btn-primary my-btn btn-start my-btn-dropdown btn-block">Edit Password</a></div>
+              </fieldset>
+          </div>
         </div>
-      </div>
-    </div><!-- /.col-->
+      </div><!-- /.col-->
   </div><!-- /.row -->
+</div>
 
 
 <?php require('Parts/footer.php'); ?>
