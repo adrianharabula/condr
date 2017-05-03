442b6c238d0e3ad94084abb14096d4a3d866ccc2
Adrian Harabula <adrian.harabula@gmail.com>
2017-05-03 22:32:18 +0300

    fucking fixes login broken redirect!!

--- app/app/Providers/AuthServiceProvider.php
+++ app/app/Providers/AuthServiceProvider.php
@@ -23,6 +23,13 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+
+        // fix Root URL
+        \URL::forceRootUrl(env('APP_URL', ''));
+        if (str_contains(env('APP_URL', ''), 'https://')) {
+            \URL::forceScheme('https');
+            //use \URL:forceSchema('https') if you use laravel < 5.4
+        }
         $this->registerPolicies();
 
         //
