152b10c2a26130479f88580e54ef300ff0b3d102
elis47 <elisa.aioanei@yahoo.com>
2017-05-07 12:05:21 +0300

    updated migrations

--- app/database/migrations/2017_06_07_000003_create_characteristics_table.php
+++ app/database/migrations/2017_06_07_000003_create_characteristics_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateCharacteristicsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,7 +15,8 @@ class CreateUsersTable extends Migration
     {
         Schema::create('characteristics', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name')->unique();
+            $table->string('name');
+            $table->string('value');
             $table->timestamps();
         });
     }
