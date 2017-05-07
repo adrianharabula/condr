50b635800addfb1f053ced3aab6f93ecd6ef6d38
Adrian Harabula <adrian.harabula@gmail.com>
2017-05-07 16:47:29 +0300

    fix class name in condrgroups migration

--- app/database/migrations/2017_05_07_000000_create_condrgroups_table.php
+++ app/database/migrations/2017_05_07_000000_create_condrgroups_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateGroupsTable extends Migration
+class CreateCondrgroupsTable extends Migration
 {
     /**
      * Run the migrations.
