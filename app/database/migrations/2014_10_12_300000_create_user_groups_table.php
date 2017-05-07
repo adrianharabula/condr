de33cc1f5c14d8647ca753a2e5f839581e156c33
elis47 <elisa.aioanei@yahoo.com>
2017-05-07 11:54:48 +0300

    Uploaded groups&user_groups migrations

---
+++ app/database/migrations/2014_10_12_300000_create_user_groups_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_groups', function (Blueprint $table) {
+            $table->increments('record_id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('group_id')->unsigned();
+            $table->foreign('group_id')->references('group_id')->on('groups')->onDelete('cascade');
+        });
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_groups');
+        Schema::disableForeignKeyConstraints();
+    }
+}
