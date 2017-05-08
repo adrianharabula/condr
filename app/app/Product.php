c83ff08bc4035ff312b960427edc24bca60fd2eb
elis47 <elisa.aioanei@yahoo.com>
2017-05-08 19:59:18 +0300

    added upc_code&image_url migration

--- app/app/Product.php
+++ app/app/Product.php
@@ -7,5 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     protected $primaryKey = "product_id";
-    
+
+    public function users() {
+      this->belongsToMany('\App\User');
+    }
 }
--- app/app/User.php
+++ app/app/User.php
@@ -27,4 +27,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function products() {
+      return this->belongsToMany('\App\Product','user_products','user_id','product_id');
+    }
 }
