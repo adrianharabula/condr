593030a50722dbdb91ba15a3a8f49933cf1d0824
Adrian Harabula <adrian.harabula@gmail.com>
2017-05-09 16:10:49 +0300

    Removes name prefix from random name

--- app/database/factories/UserFactory.php
+++ app/database/factories/UserFactory.php
@@ -4,7 +4,7 @@ $factory->define(App\User::class, function () {
     static $password;
 
     return [
-        'name' => $roFaker->name,
+        'name' => $roFaker->firstName. ' ' . $roFaker->lastName,
         'email' => $roFaker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
