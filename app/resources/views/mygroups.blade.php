6d3cc3de8d4166f4ffea58a669517f61fa35581b
Unknown <elisa.aioanei@yahoo.com>
2017-05-16 17:04:53 +0300

    added mygroups page; needs #style

--- app/resources/views/mygroups.blade.php
+++ app/resources/views/mygroups.blade.php
@@ -2,9 +2,49 @@
 
 @section('content')
 
- Hello, {{ $user->name }}.
- @foreach ($groups as $group)
-    <p>This is group {{ $group->name }} with the description {{ $group->description}}.</p>
- @endforeach
+ <div class="row">
+   <div class="col-md-6 col-md-offset-3 ">
+   	 <div class="panel">
+
+ 	     <div class="panel-heading">
+ 	      <h4><b>Change/Update your joined groups!</b></h4>
+ 	     </div>
+
+ 	     <div class="panel-body">
+
+       @foreach ($groups as $group)
+ 	      <div class="container">
+                 <div class="row">
+                     <div class="col-md-6 col-md-offset-0">
+                         <table class="table table-hover">
+                             <tbody>
+                                 <tr>
+ 									                  <td class="col-sm-8 col-md-6 col-md-offset-2">
+                                             <h5><b>Name : </b><span>{{ $group->name }}</span></h5>
+                                             <h5><b>Description : </b><span>{{ $group->description}}</span></h5>
+                                     </td>
+                                     <td class="col-md-0">
+ 				                                <div class=" col-md-offset-2">
+ 				                                    <br><a href="delete.php" style="color:green"><h5>Delete </h5></b></a>
+ 				                                </div>
+ 				                            </td>
+                                     <td class="col-md-0">
+ 				                                <div class="col-md-offset-2">
+ 				                                    <br><a href="preferences.php" style="color:green"><b><h5>Add preferences</h5></b></a>
+ 				                                </div>
+                                     </td>
+                                 </tr>
+                             </tbody>
+                         </table>
+                     </div>
+                 </div>
+             </div>
+        @endforeach
+   </div>
+
+ </div>
+
+
+
 
 @endsection
