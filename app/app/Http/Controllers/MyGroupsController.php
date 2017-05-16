17bfb0118fe70f0e18c855f168b7542b83aba6c2
Unknown <elisa.aioanei@yahoo.com>
2017-05-16 15:54:50 +0300

    get groups for authentified user #80

--- app/app/Http/Controllers/MyGroupsController.php
+++ app/app/Http/Controllers/MyGroupsController.php
@@ -3,10 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
 
 class MyGroupsController extends Controller
 {
   function index() {
-    return view('mygroups');
+    $user = Auth::user();
+    $groups = $user->groups;
+    // return $groups;
+
+    //$product = Product::find(5);
+    //$characteristics = $product->characteristics;
+
+    //$product->name;
+
+
+    return view('mygroups')->with('user', $user)
+                           ->with('groups', $groups);
   }
 }
--- app/resources/views/mygroups.blade.php
+++ app/resources/views/mygroups.blade.php
@@ -2,5 +2,9 @@
 
 @section('content')
 
+ Hello, {{ $user->name }}.
+ @foreach ($groups as $group)
+    <p>This is group {{ $group->name }} with the description {{ $group->description}}.</p>
+ @endforeach
 
 @endsection
