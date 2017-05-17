94c9811e0781870802285d8ff274f112ada96f00
Madalina-Buza <madalinabuza03@gmail.com>
2017-05-17 19:10:57 +0300

    added controller and view for products page

--- app/app/Http/Controllers/ProductsController.php
+++ app/app/Http/Controllers/ProductsController.php
@@ -3,15 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\DB;
 // use App\Product;
 
 class ProductsController extends Controller
 {
   function index() {
-    // $products = \App\Product::limit(30)->get();
+     $products = \App\Product::paginate(5);
+     //$product = \App\Product::find(1);
+     //return $p;
+     //$companies = $product->companies;
+     //return $companies;
+
+    // $company = $product->company;
+    // return $company->name;
+
+     //foreach($companies as $company)
+     //	echo $company->name. ' ';
+
     // foreach($products as $product) {
     //   echo $product->name . ' <br />';
     // }
-    return view('products');
+    return view('products')->with('products',$products);
   }
 }
--- app/resources/views/products.blade.php
+++ app/resources/views/products.blade.php
@@ -1,8 +1,67 @@
 @extends('layouts.app')
 
 @section('title','Products list')
+@section('page-colors','bg-black white')
 
 @section('content')
+<link href="/Assets/css/products.css" rel="stylesheet">
+
+<div class="container page white">
+    <div class="row">
+      <div class="col-md-12 text-center">
+          <h2>A few products...</h2>
+      </div>
+</div>
+
+    <div class="row">
+      <div class="col-md-push-8 col-md-4 black">
+        <div class="panel panel-info">
+          <div class="panel-heading">Advanced Search</div>
+          <div class="panel-body">
+            <div class="row">
+              <form class="form">
+                <div class="form-group col-md-12">
+                  <input type="text" class="form-control full-width" id="name" placeholder="Enter keywords">
+                </div>
+                <div class="form-group col-md-12">
+                  <b>Filter by</b>: <br/>
+                  <label class="radio-inline"><input type="radio" name="optradio" checked="checked">Name</label>
+                  <label class="radio-inline"><input type="radio" name="optradio">Category</label>
+                  <label class="radio-inline"><input type="radio" name="optradio">Company</label>
+                </div>
+                <div class="col-md-12">
+                  <button type="submit" class="btn btn-primary my-btn btn-block">Search</button>
+                  <br/>
+                </div>
+               </form>
+            </div>
+          </div>
+        </div>
+      </div>
+
+     <div class="col-md-8 col-md-pull-4">
+         @foreach ($products as $product)
+        <div class="row">
+            <div class="col-md-9">
+              <div class="media">
+                  <a class="thumbnail pull-left" href="viewproduct.php"> <img class="media-object" src="{{ asset($product->image_url) }}" style="width: 72px; height: 72px;"> </a>
+                  <div class="media-body">
+                       <h5><b>UPC code : </b><span>{{ $product->upc_code }}</span></h5>
+                       <h5><b>Company : </b><span>{{ $product->company->name}}</span></h5>
+                       <h5><b>Name : </b><span>{{ $product->name }}</span></h5>
+                       <h5><b>Description : </b><span>{{ $product->description}}</span></h5>
+                       <h5><b>Views : </b><span>{{ $product->views}}</span></h5>
+                  </div>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <a href="viewproduct.php" class="btn btn-primary my-btn my-btn-dropdown btn-block btn-product pull-right">View details</a>
+            </div>
+        </div>
+     @endforeach
+  </div>
+</div>
 
 
 @endsection
+ 
