ccb89181efceb7abcb5c403f5766fbb4eb38199e
Unknown <elisa.aioanei@yahoo.com>
2017-05-17 16:28:04 +0300

    added mygroups page

--- app/app/Http/Controllers/MyGroupsController.php
+++ app/app/Http/Controllers/MyGroupsController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Auth;
 
 class MyGroupsController extends Controller
 {
   function index() {
     $user = Auth::user();
-    $groups = $user->groups;
+    $groups = $user->groups()->paginate(5);
     // return $groups;
 
     //$product = Product::find(5);
@@ -17,6 +18,7 @@ class MyGroupsController extends Controller
 
     //$product->name;
 
+    // $groups = DB::table('condrgroup_user')->paginate(5);
 
     return view('mygroups')->with('user', $user)
                            ->with('groups', $groups);
--- app/resources/views/mygroups.blade.php
+++ app/resources/views/mygroups.blade.php
@@ -4,45 +4,55 @@
 
  <div class="row">
    <div class="col-md-6 col-md-offset-3 ">
-   	 <div class="panel">
+   	  <div class="panel">
+   	     <div class="panel-heading">
+   	      <h4><b>Change/Update your joined groups!</b></h4>
+   	     </div>
+   	     <div class="panel-body">
+         @foreach ($groups as $group)
+   	      <div class="container">
+                   <div class="row">
+                       <div class="col-md-6 col-md-offset-0">
+                           <table class="table table-hover">
+                               <tbody>
+                                   <tr>
+   									                  <td class="col-sm-8 col-md-6 col-md-offset-2">
+                                               <h5><b>Name : </b><span>{{ $group->name }}</span></h5>
+                                               <h5><b>Description : </b><span>{{ $group->description}}</span></h5>
+                                       </td>
+                                       <td class="col-md-0">
+   				                                <div class=" col-md-offset-2">
+   				                                    <br><a href="delete.php" style="color:green"><h5>Delete </h5></b></a>
+   				                                </div>
+   				                            </td>
+                                       <td class="col-md-0">
+   				                                <div class="col-md-offset-2">
+   				                                    <br><a href="preferences.php" style="color:green"><b><h5>Add preferences</h5></b></a>
+   				                                </div>
+                                       </td>
+                                   </tr>
+                               </tbody>
+                           </table>
+                       </div>
+                   </div>
+               </div>
+          @endforeach
+        </div>
+     </div>
 
- 	     <div class="panel-heading">
- 	      <h4><b>Change/Update your joined groups!</b></h4>
- 	     </div>
+     <nav aria-label="Product navigation" class="text-center">
+      <ul class="pagination">
+        <li class="page-item {{ $groups->currentPage() === 1 ? 'hidden' : '' }}">
+          <a class="page-link" href={{ route('mygroups') }} tabindex="-1">First</a>
+        </li>
+        {{-- <li class="page-item {{ $groups->currentPage() === 1 ? 'hidden' : '' }}">
+          <a class="page-link" href= {{ route('mygroups', ['page'=>$groups->currentPage()-1]) }} tabindex="-1">Previous</a>
+        </li> --}}
+      </ul>
+    </nav>
 
- 	     <div class="panel-body">
-
-       @foreach ($groups as $group)
- 	      <div class="container">
-                 <div class="row">
-                     <div class="col-md-6 col-md-offset-0">
-                         <table class="table table-hover">
-                             <tbody>
-                                 <tr>
- 									                  <td class="col-sm-8 col-md-6 col-md-offset-2">
-                                             <h5><b>Name : </b><span>{{ $group->name }}</span></h5>
-                                             <h5><b>Description : </b><span>{{ $group->description}}</span></h5>
-                                     </td>
-                                     <td class="col-md-0">
- 				                                <div class=" col-md-offset-2">
- 				                                    <br><a href="delete.php" style="color:green"><h5>Delete </h5></b></a>
- 				                                </div>
- 				                            </td>
-                                     <td class="col-md-0">
- 				                                <div class="col-md-offset-2">
- 				                                    <br><a href="preferences.php" style="color:green"><b><h5>Add preferences</h5></b></a>
- 				                                </div>
-                                     </td>
-                                 </tr>
-                             </tbody>
-                         </table>
-                     </div>
-                 </div>
-             </div>
-        @endforeach
-   </div>
-
- </div>
+  </div>
+</div>
 
 
 
