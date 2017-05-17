a852e8fcaf9545fc732d97d51898deee8c949fa6
Elena-Anghelina <anghelinaelena96@gmail.com>
2017-05-17 18:03:36 +0300

    update login page

--- app/resources/views/auth/login.blade.php
+++ app/resources/views/auth/login.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts.app')
 
+
 @section('content')
+
 <br />
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-6 col-md-offset-3">
             <div class="panel panel-default">
                 <div class="panel-heading">Login</div>
                 <div class="panel-body">
