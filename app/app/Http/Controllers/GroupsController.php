e53c10c079846822b85692e848bc5bf030b2cfc0
Adrian Harabula <adrian.harabula@gmail.com>
2017-05-25 01:50:36 +0300

    fix controller identation
    use 4 spaces please

--- app/app/Http/Controllers/GroupsController.php
+++ app/app/Http/Controllers/GroupsController.php
@@ -8,24 +8,26 @@ Use Auth;
 
 class GroupsController extends Controller
 {
-  function index() {
-    $groups = Group::all();
-    return view('groups')->with('groups', $groups);
-  }
+    function index() {
+        $groups = Group::all();
+        return view('groups')->with('groups', $groups);
+    }
 
-  function viewGroup(Group $group) {
-    return view('viewGroup')->with('group', $group);
-  }
+    function viewGroup(Group $group) {
+        return view('viewGroup')->with('group', $group);
+    }
 
-  function store(Group $group, Request $request) {
-    Auth::user()->groups()->syncWithoutDetaching($group);
-    $request->session()->flash('status', 'You have joined the group succesfully!');
-    return redirect()->route('viewGroup', ['id' => $group->id]);
-  }
+    function store(Group $group, Request $request) {
+        // TODO: check if user is already in the group
+        // throw an error if so
+        Auth::user()->groups()->syncWithoutDetaching($group);
+        $request->session()->flash('status', 'You have joined the group succesfully!');
+        return redirect()->route('viewGroup', ['id' => $group->id]);
+    }
 
-  function search(Request $request) {
-    $name = $request->group_name;
-    $groups = Group::search($name)->get();
-    return view('groups')->with('groups',$groups);
-  }
+    function search(Request $request) {
+        $name = $request->group_name;
+        $groups = Group::search($name)->get();
+        return view('groups')->with('groups',$groups);
+    }
 }
