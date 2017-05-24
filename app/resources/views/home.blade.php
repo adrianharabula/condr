7d47c5b70aadeb59338c67aa279301c955eb2736
Unknown <elisa.aioanei@yahoo.com>
2017-05-24 16:36:44 +0300

    fixed routes

--- app/resources/views/home.blade.php
+++ app/resources/views/home.blade.php
@@ -15,8 +15,8 @@
 
                 <div class="row">
                   <div class="col-md-6 col-md-offset-3">
-                    <a href="preferences.php" class="btn btn-primary my-btn btn-start my-btn-dropdown">Let's get started!</a>
-                    <a href="contact.php" class="btn btn-primary my-btn btn-start my-btn-dropdown">Contact us!</a>
+                    <a href={{ route('addpreferences')}} class="btn btn-primary my-btn btn-start my-btn-dropdown">Let's get started!</a>
+                    <a href={{route('contact')}} class="btn btn-primary my-btn btn-start my-btn-dropdown">Contact us!</a>
 
                   </div>
                 </div>
