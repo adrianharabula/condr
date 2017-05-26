e96aacfb473cc46add3f61ae69896b041a91c519
Adrian Harabula <adrian.harabula@gmail.com>
2017-05-26 09:30:13 +0300

    idents myproducts controller code

--- app/app/Http/Controllers/MyProductsController.php
+++ app/app/Http/Controllers/MyProductsController.php
@@ -8,25 +8,24 @@ use \App\Product as Product;
 
 class MyProductsController extends Controller
 {
-  function index()
-  {
-    $user = Auth::user();
-    // return $user->products;
-    return view('myproducts')->with('user', $user);
-  }
+    function index()
+    {
+        $user = Auth::user();
+        // return $user->products;
+        return view('myproducts')->with('user', $user);
+    }
 
-  function delete(Product $product, Request $request)
-  {
-    Auth::user()->products()->detach($product);
-    $request->session()->flash('status', 'You have deleted this product from your history!');
-    return redirect()->route('viewproduct', ['id' => $product->id]);
-  }
+    function delete(Product $product, Request $request)
+    {
+        Auth::user()->products()->detach($product);
+        $request->session()->flash('message', 'You have deleted this product from your history!');
+        return redirect()->route('viewproduct', ['id' => $product->id]);
+    }
 
-  function store(Product $product, Request $request)
-  {
-    Auth::user()->products()->syncWithoutDetaching($product);
-    $request->session()->flash('status', 'You have added this product to your history!');
-    return redirect()->route('viewproduct', ['id' => $product->id]);
-  }
-
+    function store(Product $product, Request $request)
+    {
+        Auth::user()->products()->syncWithoutDetaching($product);
+        $request->session()->flash('message', 'You have added this product to your history!');
+        return redirect()->route('viewproduct', ['id' => $product->id]);
+    }
 }
