12be9ae4fda6bb4102080eb11e0839c5210cc381
Adrian Harabula <adrian.harabula@gmail.com>
2017-05-27 13:03:16 +0300

    ads search analytics logic

---
+++ app/database/migrations/2017_05_27_005234_create_search_analytics_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSearchAnalyticsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('search_analytics', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('keyword');
+            $table->integer('number');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
