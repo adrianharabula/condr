b42f4a9170d2991c971cf69bac576cbffe2ae944
Adrian Harabula <adrian.harabula@gmail.com>
2017-05-29 01:20:08 +0300

    fix broken route

--- app/routes/web.php
+++ app/routes/web.php
@@ -56,7 +56,7 @@ Route::group(['middleware' => 'auth', 'prefix' => 'my', 'as' => 'my.'], function
         'as'   => 'products.listproducts'
     ]);
 
-    Route::any('product/{id}', [
+    Route::match(['get', 'post'], 'product/{id}', [
         'uses' => 'User\UserProductsController@addFavoriteProduct',
         'as'   => 'product.add'
     ]);
