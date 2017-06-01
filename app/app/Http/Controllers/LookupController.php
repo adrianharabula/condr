dda8b66121ba90348db6ab98f7d0c693041a68c2
Unknown <elisa.aioanei@yahoo.com>
2017-06-01 14:24:10 +0300

    added product api !!!!! + data manipulation for insert
    next: insert products into database

--- app/app/Http/Controllers/LookupController.php
+++ app/app/Http/Controllers/LookupController.php
@@ -10,37 +10,39 @@ class LookupController extends Controller
 {
     public function index ()
     {
-        // $ch = curl_init();
-        // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        //
-        // $client = new Client(['base_uri' => 'https://api.upcitemdb.com/prod/trial/lookup']);
-        // $response = $client->get('?upc=885909918188');
-        // $data = $response->getBody();
-        // $data = json_decode($data, true);
+        $client = new \GuzzleHttp\Client(array( 'curl' => array( CURLOPT_SSL_VERIFYPEER => false, ), ));
+        $request = $client->request('GET', 'https://api.upcitemdb.com/prod/trial/lookup?upc=752203011184');
+        $res = $request->getBody();
 
-        // $client = new Client();
-        // $client->setDefaultOption('verify', false);
-        // $response = $client->request('GET', 'https://api.upcitemdb.com/prod/trial/lookup?upc=885909918188');
-        // $data = $response->getBody();
-        // $data = json_decode($data, true);
-        // print_r($data);
+        $data = json_decode($res, true);
+        $items = $data['items'];
+        // print_r($items);
 
-        // print_r($data);
-        // $client = new Client(['base_uri' => 'http://pokeapi.co/api/v1/']);
-        // $response = $client->get('pokedex/1');
-        // $data = $response->getBody();
-        // $data = json_decode($data, true);
-        $client = new \GuzzleHttp\Client();
-        $res = $client->request('GET', 'https://api.github.com/repos/guzzle/guzzle');
-        echo $res->getStatusCode();
-        // 200
-        echo $res->getHeaderLine('content-type');
-        // 'application/json; charset=utf8'
-        echo $res->getBody();
-        // '{"id": 1420053, "name": "guzzle", ...}'
-
+        foreach($items as $k => $v) {
+            // echo $k . '<br>';
+            foreach ($v as $key => $value) {
+              if($key ==='images')
+              {
+                echo '<br />';
+                print_r($key);
+                foreach ($value as $p => $q)
+                {
+                    echo '<br />';
+                    print_r($p);
+                    echo '--->';
+                    print_r($q);
+                }
+              }
+              else if ($key !== 'offers')
+              {
+                    echo '<br />';
+                    print_r($key);
+                    echo '--->';
+                    print_r($value);
+              }
+            }
+        }
         // Send an asynchronous request.
-        // $request = new \GuzzleHttp\Psr7\Request('GET', 'http://httpbin.org');
         // $promise = $client->sendAsync($request)->then(function ($response) {
         //     echo 'I completed! ' . $response->getBody();
         // });
