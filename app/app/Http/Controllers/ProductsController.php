0af769870c9938c63b30d073c49569d13a0bf1b6
Adrian Harabula <adrian.harabula@gmail.com>
2017-06-03 20:55:45 +0300

    fix whitespaces for ProductsController

--- app/app/Http/Controllers/ProductsController.php
+++ app/app/Http/Controllers/ProductsController.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace App\Http\Controllers;
+
 use App\Http\Requests\ProductSearchRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -7,39 +9,6 @@ use App\Repositories\ProductRepository;
 use Illuminate\Contracts\Pagination\Paginator;
 use Auth;
 
-// class ProductsController extends Controller
-// {
-//     private $_productRepository;
-//     public function __construct(ProductRepository $products)
-//     {
-//            parent::__construct();
-//         $this->_productRepository = $products;
-//     }
-//     public function index()
-//     {
-//         $x = $this->_productRepository;
-//         dd($x);
-//         return view('products')->with('products', $this->_productRepository->getAll());
-//     }
-//     // function viewProduct(Products $products, Characteristics $cr)
-//     // {
-//     //     $crv = $cr->values($products);
-//     //     return view('products')->with('products', $products)
-//     //                           ->with('crv', $crv);
-//     // }
-//     // function search(Request $request)
-//     // {
-//     //     $name = $request->product_name;
-//     //     $products = Product::search($name)->get();
-//     //     return view('products')->with('products', $products);
-//     // }
-//     // function store(Product $products, Request $request)
-//     // {
-//     //     Auth::user()->products()->syncWithoutDetaching($products);
-//     //     $request->session()->flash('status', 'You have added this products to your history!');
-//     //     return redirect()->route('viewproduct', ['id' => $products->id]);
-//     // }
-// }
 class ProductsController extends Controller
 {
 
