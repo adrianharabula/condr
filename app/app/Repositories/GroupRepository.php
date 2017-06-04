d8d03429d32fed334ab37ff1dd8f5162c86e8db4
Adrian Harabula <adrian.harabula@gmail.com>
2017-06-04 11:04:49 +0300

    rewrite groupRepository

--- app/app/Repositories/GroupRepository.php
+++ app/app/Repositories/GroupRepository.php
@@ -2,38 +2,13 @@
 
 namespace App\Repositories;
 
-use \App\Condrgroup as Group;
 use App\Repositories\Eloquent\EloquentRepository;
-use App\SearchAnalytics;
-use stdClass;
+use \App\Condrgroup as Group;
 
 class GroupRepository extends EloquentRepository
-{
-	    /**
-     * @var \App\SearchAnalytics
-     */
-    private $_searchAnalytics;
-
-
-    /**
-     * ProductRepository constructor.
-     *
-     * @param \App\SearchAnalytics $_searchAnalytics
-     */
-    public function __construct(SearchAnalytics $_searchAnalytics)
-    {
-        parent::__construct();
-        $this->_searchAnalytics = $_searchAnalytics;
-    }
-    
+{  
     public function getModel()
     {
         return Group::class;
-    }
-
-    public function searchGroups($data)
-    {
-        $groups = $this->_model->all();
-        return $groups;
     }
 }
